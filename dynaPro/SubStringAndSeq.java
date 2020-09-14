d6b60b6acee620caaeb04f09a1f158899dbe3636
shubham sharma <shubh4profession@hotmail.com>
2020-09-14 23:31:13 +0530

    added dp solution to longestSubsequence

--- dynaPro/SubStringAndSeq.java
+++ dynaPro/SubStringAndSeq.java
@@ -53,10 +53,31 @@ public class SubStringAndSeq {
     return dp[s][e];
   }
 
+  public static int longestPalindromicSubseqTab(char[] input, boolean[][] CACHE, int[][] dp) {
+    int N = input.length;
+    for (int stringSize = 0; stringSize < N; stringSize++) {
+      for (int row = 0, col = stringSize; col < N; row++, col++) {
+        if (CACHE[row][col]) dp[row][col] = col - row + 1;
+        else if (stringSize == 2) dp[row][col] = CACHE[row][col] ? 2 : 0;
+        else if (input[row] == input[col]) dp[row][col] = dp[row + 1][col - 1] + 2;
+        else dp[row][col] = Math.max(dp[row + 1][col], dp[row][col - 1]);
+      }
+    }
+
+    int answer = 0;
+    for (int stringSize = 0; stringSize < N; stringSize++) {
+      for (int row = 0, col = stringSize; col < N; row++, col++) {
+        if (answer < dp[row][col]) answer = dp[row][col];
+      }
+    }
+
+    return answer;
+  }
+
   public static void main(String[] args) {
     // System.out.printf("longest substring is %d\n",
     // longestPalindromicSubstring("geeks4skeegabcd"));
-    String input = "aklmnma";
+    String input = "aklmnnma";
     char[] sendInput = input.toCharArray();
     int N = sendInput.length;
     int[][] dp = new int[N][N];
@@ -64,6 +85,12 @@ public class SubStringAndSeq {
     int answer = longestPalindromicSubseqMem(0, N - 1, sendInput, CACHE, dp);
     System.out.printf("longest subseq is %d\n", answer);
     for (int[] arr : dp) System.out.println(Arrays.toString(arr));
+
+    System.out.println("--------------------------------");
+    int[][] dp1 = new int[N][N];
+    answer = longestPalindromicSubseqTab(sendInput, CACHE, dp1);
+    System.out.printf("longest subseq is %d\n", answer);
+    for (int[] arr : dp1) System.out.println(Arrays.toString(arr));
   }
 
   public static void display2D(boolean[][] matrix) {
